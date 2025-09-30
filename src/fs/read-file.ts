e951c514744c4c7052443bc4ba16ff449209fc4b
Lukas <lukas.karsch03@gmail.com>
2025-09-30 22:13:36 +0200

    style: add docstring to read-file.ts

--- src/fs/read-file.ts
+++ src/fs/read-file.ts
@@ -2,4 +2,9 @@ import { PathLike, readFile, readFileSync } from "node:fs";
 
 export const readFileToJson = (path: PathLike) => readFileSync(path).toJSON();
 
+/**
+ * Read a file synchronously to a string.
+ * @param path Path to read
+ * @returns
+ */
 export const readFileToText = (path: PathLike) => readFileSync(path).toString();
